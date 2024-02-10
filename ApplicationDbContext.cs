595411a715798a40cfc1915444b722c20f6e6804
eyad <eyadmahmmoud20@gmail.com>
2024-02-11 00:14:43 +0200

    first Commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using To_Do_List.Models;
+
+namespace To_Do_List
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-A7K294K\\SQLEXPRESS;Initial Catalog=ToDoListDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+      public DbSet<Tasks> tasks { get; set; }
+    }
+}
